2d958277802c44a2644d41c0d6f3e120ed27989a
sarjid <sarjid777gmail.com>
2022-07-04 12:52:14 +0600

    support vuej3

---
+++ resources/js/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h3>This is Vue Main Component us</h3>
+    </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
-import './bootstrap';
+import "./bootstrap";
+import { createApp } from "vue";
+import App from "./App.vue";
+const app = createApp(App);
+app.mount("#app");

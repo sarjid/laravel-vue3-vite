de47c77a978bfdf48a7ed3612715758fc30c8195
sarjid <sarjid777gmail.com>
2022-07-04 19:24:15 +0600

    vuex support

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,10 @@ import "./bootstrap";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./src/router";
-// import store from "./store";
+import store from "./src/store";
 
 const app = createApp(App);
 
-app.use(router);
+app.use(router)
+app.use(store)
 app.mount("#app");
